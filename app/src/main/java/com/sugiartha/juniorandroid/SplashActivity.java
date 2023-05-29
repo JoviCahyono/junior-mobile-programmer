44d36786189f24efd4247b915949c03baf3224be
JoviCahyono <jovitj2002@gmail.com>
2023-05-29 07:19:42 +0700

    feature : add animation and some changes on splash screen

--- app/src/main/java/com/sugiartha/juniorandroid/SplashActivity.java
+++ app/src/main/java/com/sugiartha/juniorandroid/SplashActivity.java
@@ -1,11 +1,15 @@
 package com.sugiartha.juniorandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
     Handler handler;
 
@@ -14,14 +18,20 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        ImageView logoImageView = findViewById(R.id.logo_id);
+
+        // Fade-in animation for the logo
+        ObjectAnimator logoAnimator = ObjectAnimator.ofFloat(logoImageView, "alpha", 0f, 1f);
+        logoAnimator.setDuration(1000);
+        logoAnimator.setInterpolator(new AccelerateInterpolator());
+        logoAnimator.start();
+
+
         handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        handler.postDelayed(() -> {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
         },3000);
 
     }
